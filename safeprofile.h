defe9bc6d379934dd053ed294da92a55361ddf14
Thiago Adams <thiago.adams@gmail.com>
2024-08-02 14:56:12 -0300

    Update safeprofile.h

--- safeprofile.h
+++ safeprofile.h
@@ -5,7 +5,7 @@
  #pragma GCC diagnostic error "-Wdiv-by-zero"
  #pragma GCC diagnostic error "-Woverflow"
  #pragma GCC diagnostic error "-Wreturn-local-addr"
- #pragma GCC diagnostic error  "-Wstringop-overflow="
+ //#pragma GCC diagnostic error  "-Wstringop-overflow="
 
 #endif
 
