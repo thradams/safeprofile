740a6eceb2cb3b16ef2917e4cb59e9be700e0de3
Thiago Adams <thiago.adams@gmail.com>
2024-08-02 14:43:37 -0300

    Update safeprofile.h

--- safeprofile.h
+++ safeprofile.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #ifdef __GNUC__
-#pragma gcc diagnostic error "-Wdiv-by-zero"
-#pragma gcc diagnostic error "-Woverflow"
+#pragma GCC diagnostic error "-Wdiv-by-zero"
+#pragma GCC diagnostic error "-Woverflow"
+#else
+#error edit
 #endif
